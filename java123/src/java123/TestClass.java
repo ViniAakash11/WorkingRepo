6220fc25ecc6feefc87784f617278f0803cd09d8
ViniAakash11 <vinitha.varier@hotmail.com>
2023-01-26 18:53:48 +0530

    First code

---
+++ java123/src/java123/TestClass.java
@@ -0,0 +1,15 @@
+package java123;
+
+public class TestClass {
+
+	public void add()
+	{
+		System.out.println("Adding");
+	}
+	
+
+	public void sub()
+	{
+		System.out.println("Subtracting");
+	}
+}
